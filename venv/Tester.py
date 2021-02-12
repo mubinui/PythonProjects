819b7ac8b85c744a9c14f7178cc33acecee2f294
uicmu <mubin41@student.sust.edu>
2021-02-13 01:09:39 +0600

    Initial commit

---
+++ venv/Tester.py
@@ -0,0 +1,8 @@
+from mymodule import person
+from mymodule import fruits
+print(person['age'])
+print(fruits[0])
+import mymodule
+print(dir(mymodule))
+
+
---
+++ venv/mymodule.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hello, "+name)
+
+person = {
+    "name":"Mubin",
+    "age":21,
+    "Galaxy":"Milky Way"
+}
+
+fruits = list(("Mango","Apple","Grapes"))
